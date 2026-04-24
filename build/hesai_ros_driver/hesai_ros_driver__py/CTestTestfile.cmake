8e9b4119bcdce0a7b7d287c6ebf7763f551e7b79
Lukas <224311103+lukas32123@users.noreply.github.com>
2026-04-24 09:06:47 +0200

    chore: update gitignore and untrack build folders

--- build/hesai_ros_driver/hesai_ros_driver__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kilabor/isaac-go2-ros2/build/hesai_ros_driver/hesai_ros_driver__py
-# Build directory: /home/kilabor/isaac-go2-ros2/build/hesai_ros_driver/hesai_ros_driver__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/hesai_ros_driver/src/driver/HesaiLidar_SDK_2.0/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kilabor/isaac-go2-ros2/src/HesaiLidar_ROS_2.0/src/driver/HesaiLidar_SDK_2.0
-# Build directory: /home/kilabor/isaac-go2-ros2/build/hesai_ros_driver/src/driver/HesaiLidar_SDK_2.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libhesai")
